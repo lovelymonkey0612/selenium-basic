0eb6c69ae36d5cd072bc394569372b7d3b3ca414
Denis Martin <denysmartin4952@gmail.com>
2024-01-27 15:18:15 -0800

    console url log

--- google.py
+++ google.py
@@ -5,11 +5,11 @@ from selenium import webdriver
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
 
 driver.get('http://www.google.com/')
-
+print(driver.title)
 time.sleep(5) # Let the user actually see something!
 
 search_box = driver.find_element('name', 'q')
-
+print(driver.current_url)
 search_box.send_keys('ChromeDriver')
 
 search_box.submit()
